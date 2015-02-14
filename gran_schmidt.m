0d4f932ae9fac7ee32306b0245922e37f1c87d15
Michael Akintunde <michael.akintunde13@imperial.ac.uk>
2015-02-14 17:24:53 +0000

    remove comments and unused variables

--- gran_schmidt.m
+++ gran_schmidt.m
@@ -3,15 +3,14 @@ function[result] = gran_schmidt(varargin)
     A = cell2mat(A_raw);
     V(:,1) = A(:, 1);
     Q(:,1) = V(:,1) / norm(V(:,1));
-    %args = zeros(size(A, 1),1);
     for i=2:nargin,
-        V(:,i) = A(:, i) - sum(A, Q, i, nargin);
+        V(:,i) = A(:, i) - sum(A, Q, i);
         Q(:,i) = V(:, i) / norm(V(:,i));
     end
     result = (Q' * Q) - eye(nargin);
 end
 
-function[result] = sum(A, Q, i, n)
+function[result] = sum(A, Q, i)
     R = zeros(size(A, 1), 1);
     for j=1:i-1,
         R = R + dot(A(:,i), Q(:,j)) * Q(:,j);
